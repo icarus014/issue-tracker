60ed965d8af2c7937a5c835567f276649b3594a0
Darian Brown <browndarian011@gmail.com>
2023-11-03 13:03:58 -0400

    added notes and li fucntion

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+'use client';
 import React from 'react'
 import Link from 'next/link'
 import { AiFillBug } from 'react-icons/ai'
+import { usePathname } from 'next/navigation'
+import classnames from 'classnames';
 
 const NavBar = () => {
+
+     const currentPath = usePathname()
+    //  console.log(currentPath) (checking the right path when selected in console)
+
     const links = [
         {label: 'Dashboard', href:'/'},
         {label: 'Issues', href:'/issues'},
@@ -10,11 +17,16 @@ const NavBar = () => {
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center text-black text-lg'>
         <Link href="/"> <AiFillBug/> </Link>
+        {/* allows me to multiple labels w/o have multiple li components */}
         <ul className='flex space-x-6'>
             {links.map(link => 
             <Link 
             key={link.href} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+            className={classnames({
+                'text-zinc-900': link.href === currentPath, //equals
+                'text-zinc-500' : link.href !== currentPath, //does not equal 
+                'hover:text-zinc-800 transition-colors' : true //allows hover 
+            })} 
             href={link.href}>{link.label}</Link>)}
 
         </ul>
