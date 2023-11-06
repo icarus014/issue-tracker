9ce82b3e02c3302f07c92dd11c96544b170cae01
Darian Brown <browndarian011@gmail.com>
2023-11-06 13:48:44 -0500

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,17 +2,40 @@
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from 'axios'
+import { useForm, Controller } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+interface IssueForm{
+    title: string;
+    description: string;
+}
 
 
 const NewIssuePage = () => {
+    const router = useRouter()
+    const {register, control, handleSubmit} = useForm<IssueForm>();
+    // console.log(register('title'))
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form 
+        className="max-w-xl space-y-3" 
+        // this returns  promise, which requires the use of "await" and "async"
+        onSubmit={handleSubmit( async(data) => {
+            await axios.post('/api/issues', data);
+            router.push('/issues')
+            })}>
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register('title')} /> 
+        {/* using props for components  */}
       </TextField.Root>
-      <SimpleMDE placeholder="Description"/>
+      <Controller 
+        name="description"
+        control={control}
+        render={({field})=><SimpleMDE placeholder="Description" {...field}/>}
+        />
       <Button>Submit new Issue</Button>
-    </div>
+    </form>
   );
 };
 
