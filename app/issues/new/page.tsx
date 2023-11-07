02cce2e54c87a0c6c7bf66e6411e1f02cd1c8e91
Darian Brown <browndarian011@gmail.com>
2023-11-07 18:01:56 -0500

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,41 +1,56 @@
-'use client'
-import { Button, TextField } from "@radix-ui/themes";
+"use client";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import axios from 'axios'
+import axios from "axios";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-interface IssueForm{
-    title: string;
-    description: string;
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-
 const NewIssuePage = () => {
-    const router = useRouter()
-    const {register, control, handleSubmit} = useForm<IssueForm>();
-    // console.log(register('title'))
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
+  // console.log(register('title'))
 
   return (
-    <form 
-        className="max-w-xl space-y-3" 
+    <div className="max-w-xl space-y-3 mb-5">
+      {error && (
+        <Callout.Root color="blue">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        
         // this returns  promise, which requires the use of "await" and "async"
-        onSubmit={handleSubmit( async(data) => {
-            await axios.post('/api/issues', data);
-            router.push('/issues')
-            })}>
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register('title')} /> 
-        {/* using props for components  */}
-      </TextField.Root>
-      <Controller 
-        name="description"
-        control={control}
-        render={({field})=><SimpleMDE placeholder="Description" {...field}/>}
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error has occurred");
+          }
+        })}
+      >
+        <TextField.Root className="mb-3">
+          <TextField.Input placeholder="Title" {...register("title")} />
+          {/* using props for components  */}
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
         />
-      <Button>Submit new Issue</Button>
-    </form>
+        <Button>Submit new Issue</Button>
+      </form>
+    </div>
   );
 };
 
